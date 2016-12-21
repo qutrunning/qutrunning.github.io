996b2a3881c47e9f0a706dc7e8239edd281aa973
Tim Jones <mynameistim97@gmail.com>
2016-12-22 00:29:30 +1000

    Updated site wallpaper, changed fade in point

--- public/js/app.js
+++ public/js/app.js
@@ -121,7 +121,7 @@ $(document).ready(function() {
 	            var bottom_of_window = $(window).scrollTop() + $(window).height();
 	            
 	            /* If the object is completely visible in the window, fade it in */
-	            if( bottom_of_window > (bottom_of_object * 0.95)){
+	            if( bottom_of_window > (bottom_of_object * 0.9)){
 
 	                $(this).animate({'opacity':'1'},400);             
 	            }            
@@ -132,18 +132,29 @@ $(document).ready(function() {
 	            var bottom_of_window = $(window).scrollTop() + $(window).height();
 	            
 	            /* If the object is completely visible in the window, fade it in */
-	            if( bottom_of_window > (bottom_of_object * 0.95)){
+	            if( bottom_of_window > (bottom_of_object * 0.7)){
 
 	                $(this).animate({'opacity':'1'},400);             
 	            }            
-	        });     
+	        }); 
+          $('.centered-overview').each( function(i){
+              
+              var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+              var bottom_of_window = $(window).scrollTop() + $(window).height();
+              
+              /* If the object is completely visible in the window, fade it in */
+              if( bottom_of_window > (bottom_of_object * 0.875)){
+
+                  $(this).animate({'opacity':'1'},400);             
+              }            
+          });         
 	        $('.centered-contact').each( function(i){
 	            
 	            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
 	            var bottom_of_window = $(window).scrollTop() + $(window).height();
 	            
 	            /* If the object is completely visible in the window, fade it in */
-	            if( bottom_of_window > (bottom_of_object * 0.95)){
+	            if( bottom_of_window > (bottom_of_object * 0.7)){
 
 	                $(this).animate({'opacity':'1'},400);             
 	            }            
