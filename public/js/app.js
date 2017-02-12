0d8803d1d045d7f0ad496e3d9f2a675c8f9b20df
Tim Jones <mynameistim97@gmail.com>
2017-02-12 17:38:31 +1000

    Updated site wallpaper with svg file, modified where beginning of fadeout effect starts for carousel

--- public/js/app.js
+++ public/js/app.js
@@ -196,7 +196,7 @@ $(document).ready(function() {
 //Script to fade out the Carousel when scrolled past
 $(window).scroll(function () {
     var scrollTop = $(window).scrollTop();
-    var height = ($(window).height() / 1); //1.7 previous
+    var height = ($(window).height() / 0.7); //1.7 previous
     $('.first-block').css({
 
         'opacity': ((height - scrollTop) / (height))
