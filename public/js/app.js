bf9aea4a35a02ee7ed0aab42752205688092ab28
DESKTOP-JKTH7EG\Tim <mynameistim97@gmail.com>
2017-07-07 09:31:15 +1000

    Added JQuery code to initialise video only after page and JavaScript load

--- public/js/app.js
+++ public/js/app.js
@@ -445,3 +445,13 @@ $(document).ready(function() {
     }
   });
 });
+
+video = jQuery('#video').get()[0];
+
+video.addEventListener('loadeddata', function() {
+    video.play();
+});
+
+video.addEventListener('pause', function() {
+    video.play();
+});
